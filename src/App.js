8b563f47115c257f142237f87fc23a7260117aa7
alan <alan_fgc@outlook.com>
2023-02-24 20:52:54 -0500

    Finished lab portion of a6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from 'react-router-dom';
+import {Routes, Route} from "react-router";
 function App() {
   return (
+      <BrowserRouter>
       <div className="container">
-        <h1>Hello World!</h1>
-        <Labs/>
+          <Routes>
+              <Route path="/"
+                     element={<Labs/>}/>
+              <Route path="/hello"
+                     element={<HelloWorld/>}/>
+              <Route path="/tuiter"
+                     element={<Tuiter/>}/>
+          </Routes>
       </div>
-  );
+    </BrowserRouter>
+);
 }
 
 
